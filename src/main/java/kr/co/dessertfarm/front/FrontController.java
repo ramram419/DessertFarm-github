3372cc6316978bc86b6f092b4f8c2d6f40317449
ramram419 <barbara0419@naver.com>
2021-08-23 00:07:57 +0900

    add newitem, categoryitem

--- src/main/java/kr/co/dessertfarm/front/FrontController.java
+++ src/main/java/kr/co/dessertfarm/front/FrontController.java
@@ -47,6 +47,16 @@ public class FrontController {
 		return "home/contents/QnAlist";
 	}
 	
+	@RequestMapping("/newitem")
+	public String newitem() {
+		return "home/contents/newitem";
+	}
+	
+	@RequestMapping("/category")
+	public String category() {
+		return "home/contents/category";
+	}
+	
 	@RequestMapping(value = "/welcome/client", method= {RequestMethod.GET, RequestMethod.POST})
 	public String client_join(HttpServletRequest req) {
 		MainVO vo = new MainVO();
