ee8e8df62046017550e3ab83938ec43fac66c039
Jin-seung-beom <jinsb1999@naver.com>
2021-08-24 22:32:43 +0900

    test commit

--- src/main/java/kr/co/dessertfarm/config/MainConfig.java
+++ src/main/java/kr/co/dessertfarm/config/MainConfig.java
@@ -17,7 +17,6 @@ import kr.co.dessertfarm.article.ArticleDAO;
 import kr.co.dessertfarm.article.ArticleService;
 import kr.co.dessertfarm.join.JoinService;
 import kr.co.dessertfarm.login.LoginService;
-import kr.co.dessertfarm.product.ProductDAO;
 import kr.co.dessertfarm.product.ProductService;
 import kr.co.dessertfarm.spring.MainDAO;
 import kr.co.dessertfarm.spring.ProductDAO;
@@ -69,11 +68,6 @@ public class MainConfig {
 	@Bean
 	public ArticleDAO articleDAO() {
 		return new ArticleDAO();
-	}
-	
-	@Bean
-	public ProductDAO productDAO() {
-		return new ProductDAO();
 	}
 	
 	@Bean
